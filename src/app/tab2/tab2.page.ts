43b3f6ef7cec2a82ed9d3125b24a1a32cb439eb1
Johan Vargas <johaniza1212@mail.com>
2025-10-06 12:23:44 -0500

    Completado el curriculum de ambos compañeros

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,50 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle,
+  IonCardSubtitle, IonCardContent, IonFooter, IonAvatar, IonLabel, IonItem, IonList,
+  IonIcon, IonChip, IonSegment, IonSegmentButton, IonButtons, IonButton, IonAccordionGroup,
+  IonAccordion, IonProgressBar, IonFab, IonFabButton, IonGrid, IonRow, IonCol
+} from '@ionic/angular/standalone';
+
+import {
+  personCircleOutline,
+  globeOutline,
+  sendOutline,
+  codeSlashOutline,
+  barChartOutline,
+  chatbubblesOutline,
+  logoGithub
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle,
+    IonCardSubtitle, IonCardContent, IonFooter, IonAvatar, IonLabel, IonItem, IonList,
+    IonIcon, IonChip, IonSegment, IonSegmentButton, IonButtons, IonButton, IonAccordionGroup,
+    IonAccordion, IonProgressBar, IonFab, IonFabButton, IonGrid, IonRow, IonCol
+  ],
 })
 export class Tab2Page {
+  activeSection = 'perfil';
 
-  constructor() {}
+  constructor() {
+    addIcons({
+      'person-circle-outline': personCircleOutline,
+      'globe-outline': globeOutline,
+      'send-outline': sendOutline,
+      'code-slash-outline': codeSlashOutline,
+      'bar-chart-outline': barChartOutline,
+      'chatbubbles-outline': chatbubblesOutline,
+      'logo-github': logoGithub
+    });
+  }
 
+  onSegmentChange(event: CustomEvent) {
+    this.activeSection = event.detail.value;
+  }
 }
